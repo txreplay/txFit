e05d1cecaa4571c58065ddc979e9654451131c0e
txreplay <txreplay@gmail.com>
2019-09-29 18:26:39 +0200

    get/post sessions

--- app/src/main/java/com/txreplay/txfit/MainActivity.java
+++ app/src/main/java/com/txreplay/txfit/MainActivity.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.fitness.Fitness;
+import com.google.android.gms.fitness.FitnessActivities;
 import com.google.android.gms.fitness.FitnessOptions;
+import com.google.android.gms.fitness.data.DataPoint;
+import com.google.android.gms.fitness.data.DataSet;
+import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
+import com.google.android.gms.fitness.data.Field;
+import com.google.android.gms.fitness.data.Session;
 import com.google.android.gms.fitness.request.DataReadRequest;
+import com.google.android.gms.fitness.request.SessionInsertRequest;
+import com.google.android.gms.fitness.request.SessionReadRequest;
 import com.google.android.gms.fitness.result.DataReadResponse;
+import com.google.android.gms.fitness.result.SessionReadResponse;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -26,12 +36,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static java.text.DateFormat.getTimeInstance;
 
 public class MainActivity extends AppCompatActivity {
     public static final String LOG_TAG = "BasicSessions";
+    public static final String SAMPLE_SESSION_NAME = "txFitSession";
 
     private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
 
@@ -108,6 +123,254 @@ public class MainActivity extends AppCompatActivity {
                         Log.d(LOG_TAG, "onComplete()");
                     }
                 });
+    }
+
+    /**
+     *  Creates and executes a {@link SessionInsertRequest} using {@link
+     *  com.google.android.gms.fitness.SessionsClient} to insert a session.
+     */
+    private Task<Void> insertSession() {
+        //First, create a new session and an insertion request.
+        SessionInsertRequest insertRequest = insertFitnessSession();
+
+        // [START insert_session]
+        // Then, invoke the Sessions API to insert the session and await the result,
+        // which is possible here because of the AsyncTask. Always include a timeout when
+        // calling await() to avoid hanging that can occur from the service being shutdown
+        // because of low memory or other conditions.
+        Log.i(LOG_TAG, "Inserting the session in the Sessions API");
+        return Fitness.getSessionsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .insertSession(insertRequest)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // At this point, the session has been inserted and can be read.
+                        Log.i(LOG_TAG, "Session insert was successful!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(LOG_TAG, "There was a problem inserting the session: " +
+                                e.getLocalizedMessage());
+                    }
+                });
+        // [END insert_session]
+    }
+
+    /**
+     *  Creates and executes a {@link SessionReadRequest} using {@link
+     *  com.google.android.gms.fitness.SessionsClient} to verify the insertion succeeded .
+     */
+    private Task<SessionReadResponse> verifySession() {
+        // Begin by creating the query.
+        SessionReadRequest readRequest = readFitnessSession();
+
+        // [START read_session]
+        // Invoke the Sessions API to fetch the session with the query and wait for the result
+        // of the read request. Note: Fitness.SessionsApi.readSession() requires the
+        // ACCESS_FINE_LOCATION permission.
+        return Fitness.getSessionsClient(this, GoogleSignIn.getLastSignedInAccount(this))
+                .readSession(readRequest)
+                .addOnSuccessListener(new OnSuccessListener<SessionReadResponse>() {
+                    @Override
+                    public void onSuccess(SessionReadResponse sessionReadResponse) {
+                        // Get a list of the sessions that match the criteria to check the result.
+                        List<Session> sessions = sessionReadResponse.getSessions();
+                        Log.i(LOG_TAG, "Session read was successful. Number of returned sessions is: "
+                                + sessions.size());
+
+                        for (Session session : sessions) {
+                            // Process the session
+                            dumpSession(session);
+
+                            // Process the data sets for this session
+                            List<DataSet> dataSets = sessionReadResponse.getDataSet(session);
+                            for (DataSet dataSet : dataSets) {
+                                dumpDataSet(dataSet);
+                            }
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i(LOG_TAG, "Failed to read session");
+                    }
+                });
+        // [END read_session]
+    }
+
+    /**
+     *  Inserts and verifies a session by chaining {@link Task} form {@link #insertSession} and
+     *  {@link #verifySession}.
+     */
+    private void insertAndVerifySession() {
+
+        insertSession().continueWithTask(new Continuation<Void, Task<SessionReadResponse>>() {
+            @Override
+            public Task<SessionReadResponse> then(@NonNull Task<Void> task) throws Exception {
+                return verifySession();
+            }
+        });
+    }
+
+    /**
+     *  Creates a {@link SessionInsertRequest} for a run that consists of 10 minutes running,
+     *  10 minutes walking, and 10 minutes of running. The request contains two {@link DataSet}s:
+     *  speed data and activity segments data.
+     *
+     *  {@link Session}s are time intervals that are associated with all Fit data that falls into
+     *  that time interval. This data can be inserted when inserting a session or independently,
+     *  without affecting the association between that data and the session. Future queries for
+     *  that session will return all data relevant to the time interval created by the session.
+     *
+     *  Sessions may contain {@link DataSet}s, which are comprised of {@link DataPoint}s and a
+     *  {@link DataSource}.
+     *  A {@link DataPoint} is associated with a Fit {@link DataType}, which may be
+     *  derived from the {@link DataSource}, as well as a time interval, and a value. A given
+     *  {@link DataSet} may only contain data for a single data type, but a {@link Session} can
+     *  contain multiple {@link DataSet}s.
+     */
+    private SessionInsertRequest insertFitnessSession() {
+        Log.i(LOG_TAG, "Creating a new session for an afternoon run");
+        // Setting start and end times for our run.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        // Set a range of the run, using a start time of 30 minutes before this moment,
+        // with a 10-minute walk in the middle.
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.MINUTE, -10);
+        long endWalkTime = cal.getTimeInMillis();
+        cal.add(Calendar.MINUTE, -10);
+        long startWalkTime = cal.getTimeInMillis();
+        cal.add(Calendar.MINUTE, -10);
+        long startTime = cal.getTimeInMillis();
+
+        // Create a data source
+        DataSource speedDataSource = new DataSource.Builder()
+                .setAppPackageName(this.getPackageName())
+                .setDataType(DataType.TYPE_SPEED)
+                .setName(SAMPLE_SESSION_NAME + "- speed")
+                .setType(DataSource.TYPE_RAW)
+                .build();
+
+        float runSpeedMps = 10;
+        float walkSpeedMps = 3;
+        // Create a data set of the run speeds to include in the session.
+        DataSet speedDataSet = DataSet.create(speedDataSource);
+
+        DataPoint firstRunSpeed = speedDataSet.createDataPoint()
+                .setTimeInterval(startTime, startWalkTime, TimeUnit.MILLISECONDS);
+        firstRunSpeed.getValue(Field.FIELD_SPEED).setFloat(runSpeedMps);
+        speedDataSet.add(firstRunSpeed);
+
+        DataPoint walkSpeed = speedDataSet.createDataPoint()
+                .setTimeInterval(startWalkTime, endWalkTime, TimeUnit.MILLISECONDS);
+        walkSpeed.getValue(Field.FIELD_SPEED).setFloat(walkSpeedMps);
+        speedDataSet.add(walkSpeed);
+
+        DataPoint secondRunSpeed = speedDataSet.createDataPoint()
+                .setTimeInterval(endWalkTime, endTime, TimeUnit.MILLISECONDS);
+        secondRunSpeed.getValue(Field.FIELD_SPEED).setFloat(runSpeedMps);
+        speedDataSet.add(secondRunSpeed);
+
+        // [START build_insert_session_request_with_activity_segments]
+        // Create a second DataSet of ActivitySegments to indicate the runner took a 10-minute walk
+        // in the middle of the run.
+        DataSource activitySegmentDataSource = new DataSource.Builder()
+                .setAppPackageName(this.getPackageName())
+                .setDataType(DataType.TYPE_ACTIVITY_SEGMENT)
+                .setName(SAMPLE_SESSION_NAME + "-activity segments")
+                .setType(DataSource.TYPE_RAW)
+                .build();
+        DataSet activitySegments = DataSet.create(activitySegmentDataSource);
+
+        DataPoint firstRunningDp = activitySegments.createDataPoint()
+                .setTimeInterval(startTime, startWalkTime, TimeUnit.MILLISECONDS);
+        firstRunningDp.getValue(Field.FIELD_ACTIVITY).setActivity(FitnessActivities.RUNNING);
+        activitySegments.add(firstRunningDp);
+
+        DataPoint walkingDp = activitySegments.createDataPoint()
+                .setTimeInterval(startWalkTime, endWalkTime, TimeUnit.MILLISECONDS);
+        walkingDp.getValue(Field.FIELD_ACTIVITY).setActivity(FitnessActivities.WALKING);
+        activitySegments.add(walkingDp);
+
+        DataPoint secondRunningDp = activitySegments.createDataPoint()
+                .setTimeInterval(endWalkTime, endTime, TimeUnit.MILLISECONDS);
+        secondRunningDp.getValue(Field.FIELD_ACTIVITY).setActivity(FitnessActivities.RUNNING);
+        activitySegments.add(secondRunningDp);
+
+        // [START build_insert_session_request]
+        // Create a session with metadata about the activity.
+        Session session = new Session.Builder()
+                .setName(SAMPLE_SESSION_NAME)
+                .setDescription("Long run around Shoreline Park")
+                .setIdentifier("UniqueIdentifierHere")
+                .setActivity(FitnessActivities.RUNNING)
+                .setStartTime(startTime, TimeUnit.MILLISECONDS)
+                .setEndTime(endTime, TimeUnit.MILLISECONDS)
+                .build();
+
+        // Build a session insert request
+        SessionInsertRequest insertRequest = new SessionInsertRequest.Builder()
+                .setSession(session)
+                .addDataSet(speedDataSet)
+                .addDataSet(activitySegments)
+                .build();
+        // [END build_insert_session_request]
+        // [END build_insert_session_request_with_activity_segments]
+
+        return insertRequest;
+    }
+
+    /**
+     * Returns a {@link SessionReadRequest} for all speed data in the past week.
+     */
+    private SessionReadRequest readFitnessSession() {
+        Log.i(LOG_TAG, "Reading History API results for session: " + SAMPLE_SESSION_NAME);
+        // [START build_read_session_request]
+        // Set a start and end time for our query, using a start time of 1 week before this moment.
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        long startTime = cal.getTimeInMillis();
+
+        // Build a session read request
+        SessionReadRequest readRequest = new SessionReadRequest.Builder()
+                .setTimeInterval(startTime, endTime, TimeUnit.MILLISECONDS)
+                .read(DataType.TYPE_SPEED)
+                .setSessionName(SAMPLE_SESSION_NAME)
+                .build();
+        // [END build_read_session_request]
+
+        return readRequest;
+    }
+
+    private void dumpDataSet(DataSet dataSet) {
+        Log.i(LOG_TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+        for (DataPoint dp : dataSet.getDataPoints()) {
+            DateFormat dateFormat = getTimeInstance();
+            Log.i(LOG_TAG, "Data point:");
+            Log.i(LOG_TAG, "\tType: " + dp.getDataType().getName());
+            Log.i(LOG_TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+            Log.i(LOG_TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+            for(Field field : dp.getDataType().getFields()) {
+                Log.i(LOG_TAG, "\tField: " + field.getName() +
+                        " Value: " + dp.getValue(field));
+            }
+        }
+    }
+
+    private void dumpSession(Session session) {
+        DateFormat dateFormat = getTimeInstance();
+        Log.i(LOG_TAG, "Data returned for Session: " + session.getName()
+                + "\n\tDescription: " + session.getDescription()
+                + "\n\tStart: " + dateFormat.format(session.getStartTime(TimeUnit.MILLISECONDS))
+                + "\n\tEnd: " + dateFormat.format(session.getEndTime(TimeUnit.MILLISECONDS)));
     }
 
     @Override
